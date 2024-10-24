74378e95f213b6e7c1972f3999f236057f1276fc
akash <dangudubiyyapuakash@gmail.com>
2024-10-24 22:11:36 +0530

    final updated

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = process.env.NEXT_PUBLIC_SERVER_URL;
+export const HOST = 'https://flexigigs-server.onrender.com';
 export const API_URL = `${HOST}/api`;
 export const IMAGES_URL = `${HOST}/uploads`;
 
