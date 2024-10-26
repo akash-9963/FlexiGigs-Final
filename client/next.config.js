8fcec6576da4bc0d315cc9523859634b2cb9afd6
akash <dangudubiyyapuakash@gmail.com>
2024-10-26 22:31:40 +0530

    nextjs

--- client/next.config.js
+++ client/next.config.js
@@ -4,20 +4,19 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8747",
+        protocol: "https",
+        hostname: "flexigigs-server.onrender.com", // Ensure the correct protocol and hostname for your image server
+        port: "", // No port needed for default ports
       },
     ],
   },
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        source: "/api/:path*", // Matching all API routes
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://flexigigs.vercel.app" }, // Consider specifying your frontend's URL for security
+          { key: "Access-Control-Allow-Origin", value: "https://flexigigs.vercel.app" }, // Your frontend URL
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ],
